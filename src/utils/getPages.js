e58305793c68952ddce25b143f1e34ffd282b0a4
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-09-29 01:07:53 +0300

    display pics

--- src/utils/getPages.js
+++ src/utils/getPages.js
@@ -1,6 +1,6 @@
-export const getPages = () => {
+export const getPages = (page) => {
   const url =
-    "https://api.unsplash.com/search/collections?page=2&limit=10&query=cat&client_id=kQ_rA8Dd9Tb-JZ80Nx6RyFBtaoIFyaP5kdLn5EmGkVM";
+    `https://api.unsplash.com/search/collections?page=${page}&limit=20&query=cat&client_id=kQ_rA8Dd9Tb-JZ80Nx6RyFBtaoIFyaP5kdLn5EmGkVM`;
   return fetch(url)
     .then((res) => res.json())
     .catch((err) => {
